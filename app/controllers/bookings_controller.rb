3c9e087dc66080a672dfb07674aa698c3e2754cc
owen-makes <owen.botto@gmail.com>
2024-11-09 00:29:18 -0300

    Add ticketing flow

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,34 @@
 class BookingsController < ApplicationController
   def new
+    @flight = Flight.find(params[:flight_id])
+    @booking = Booking.new(flight: @flight)
+    params[:seats_available].to_i.times { @booking.passengers.build }
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+
+    if @booking.save
+      redirect_to @booking, notice: "Booking was successfully created."
+    else
+      @flight = @booking.flight
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to flights_path, alert: "Booking not found"
+  end
+
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(
+      :flight_id,
+      passengers_attributes: [ :name, :email ]
+    )
   end
 end
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,3 +1,5 @@
 class Passenger < ApplicationRecord
   has_and_belongs_to_many :bookings
+  validates :name, :email, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
